f0bc42d29fe446e622fbff5083fb13c1271e92e5
Higuaco <higuu28@gmail.com>
2021-07-29 00:18:20 +0900

    jQuery実装途中
    楽しい

---
+++ script.js
@@ -0,0 +1,43 @@
+//logoの表示
+$(window).on('load',function(){
+  $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+  $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+});
+
+// ------------------ページ遷移------------------//
+
+$('a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top;	//idの上部の距離を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $('.fadeInUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeInUp');
+    // 画面内に入ったらfadeInUpというクラス名を追記
+    }
+    // 画面外に出たらfadeInUpというクラス名を外す
+    // else{
+    //   $(this).removeClass('fadeIn');
+    // }
+    });
+  }//ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+  // 画面をスクロールをしたら動かしたい場合の記述
+    $(window).scroll(function (){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+  // 画面が読み込まれたらすぐに動かしたい場合の記述
+    $(window).on('load', function(){
+      fadeAnime();/* アニメーション用の関数を呼ぶ*/
+    });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
