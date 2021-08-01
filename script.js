a736af752170e4143f6dd1f71e4080b76fe2f33d
Higuaco <higuu28@gmail.com>
2021-08-01 14:39:45 +0900

    注文拡大画面完成
    次からPHP

--- script.js
+++ script.js
@@ -38,3 +38,30 @@ function fadeAnime(){
   $(window).on('load', function(){
     fadeAnime();/* アニメーション用の関数を呼ぶ*/
   });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+// ------------------ショッピング 選択画像拡大------------------//
+//上部画像の設定
+$('.gallery2').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	fade: true, //フェードの有効化
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+});
+
+//選択画像の設定
+$('.choice-btn').slick({
+	infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 3, //表示させるスライドの数
+	focusOnSelect: true, //フォーカスの有効化
+	asNavFor: '.gallery2', //連動させるスライドショーのクラス名
+});
+
+//下の選択画像をスライドさせずに連動して変更させる設定。
+$('.gallery2').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+	var index = nextSlide; //次のスライド番号
+	//サムネイルのslick-currentを削除し次のスライド要素にslick-currentを追加
+	$(".choice-btn .slick-slide").removeClass("slick-current").eq(index).addClass("slick-current");
+});
