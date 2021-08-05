4986f0e545f3d1db8c152c04103b920b0becf0f0
Higuaco <higuu28@gmail.com>
2021-08-05 22:49:28 +0900

    contact 完成
    cssもオッケー

--- contact.php
+++ contact.php
@@ -97,7 +97,7 @@ $ticket = $_SESSION[ 'ticket' ];
       <div class="con-phase">
         <form id="main_contact" method="post" action="confirm.php">
           <!-- 〓〓〓〓〓〓 -->
-          <div class="form-group">
+          <div class="form-group ta-l">
             <label for="subject">お問い合わせの種類
               <span class="error"><?php echo h( $error_subject ); ?></span>
             </label>
@@ -138,7 +138,7 @@ $ticket = $_SESSION[ 'ticket' ];
           </div>
           <!-- 〓〓〓〓〓〓 -->
           <div class="form-group">
-            <label for="tel">お電話番号 *半角英数字）
+            <label for="tel">お電話番号 *半角英数字
               <span class="error"><?php echo h( $error_tel ); ?></span>
               <span class="error"><?php echo h( $error_tel_format ); ?></span>
             </label>
@@ -150,114 +150,147 @@ $ticket = $_SESSION[ 'ticket' ];
             <label for="body">お問い合わせ内容
               <span class="error"><?php echo h( $error_body ); ?></span>
             </label>
-            <span id="count"> </span>/1000
+            <div class="d-b">
+              <span id="count"> </span>/1000
+            </div>
             <textarea class="form-control validate max1000 required" id="body" name="body"
               placeholder="お問い合わせ内容（1000文字まで）を書きください" rows="3"><?php echo h($body); ?></textarea>
           </div>
       </div>
       <!-- 〓〓〓〓〓〓 -->
-      <button type="submit" class="btn btn-primary">確認画面へ</button>
-      <!--確認ページへトークンをPOSTする、隠しフィールド「ticket」-->
-      <input type="hidden" name="ticket" value="<?php echo h($ticket); ?>">
+      <div class="btn-8">
+        <button type="submit" class="btn btn-primary">確認画面へ</button>
+        <!--確認ページへトークンをPOSTする、隠しフィールド「ticket」-->
+        <input type="hidden" name="ticket" value="<?php echo h($ticket); ?>">
+      </div>
       </form>
     </div>
     <!-- reCAPTCHA ウィジェットを表示する場合は以下のコメントアウトを外す -->
     <!--<script src="https://www.google.com/recaptcha/api.js?render=<?php echo $siteKey; ?>"></script>-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script>
-    jQuery(function($) {
 
-      //エラーを表示する関数（error クラスの p 要素を追加して表示）
-      function show_error(message, this$) {
-        text = this$.parent().find('label').text() + message;
-        this$.parent().append("<p class='error'>" + text + "</p>");
+    <!-- footer -->
+    <div class="main_contact">
+      <img loading="lazy" class="main_con_i fadeUpTrigger" src="img/u-cafe-logo_black.png" alt="ロゴ黒">
+      <p class="main_con_p fadeUpTrigger">You and Me Café</p>
+      <div class="main_cons d-f jc-c fadeUpTrigger">
+        <div class="main_con_i_1"></div>
+        <div class="main_con_i_2"></div>
+      </div>
+      <div class="btn-5 fadeUpTrigger">
+        <a href="contact.html">
+          <div class="con_1 fadeUpTrigger">
+            <h3>Contact</h3>
+            <p class="con_2">U-cafeへのお問い合わせはこちらから</p>
+          </div>
+        </a>
+      </div>
+      <a class="hov" href="/"><img loading="lazy" class="con_3 fadeUpTrigger" src="img/u-cafe_maki.png" alt=""></a>
+      <div class="inner">
+        <div class="con_4 fadeUpTrigger"></div>
+      </div>
+    </div>
+  </main>
+  <footer class="fadeUpTrigger">
+    <small>Copyright &copy Sheepontaneous,All rights reserved.</small>
+  </footer>
+  <script src="script.js"></script>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script>
+  jQuery(function($) {
+
+    //エラーを表示する関数（error クラスの p 要素を追加して表示）
+    function show_error(message, this$) {
+      text = this$.parent().find('label').text() + message;
+      this$.parent().append("<p class='error'>" + text + "</p>");
+    }
+
+    //フォームが送信される際のイベントハンドラの設定
+    $("#main_contact").submit(function() {
+      //エラー表示の初期化
+      $("p.error").remove();
+      $("div").removeClass("error");
+      var text = "";
+      $("#errorDispaly").remove();
+
+      //メールアドレスの検証
+      var email = $.trim($("#email").val());
+      if (email && !(/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/gi).test(email)) {
+        $("#email").after("<p class='error'>メールアドレスの形式が異なります</p>");
+      }
+      //確認用メールアドレスの検証
+      var email_check = $.trim($("#email_check").val());
+      if (email_check && email_check != $.trim($("input[name=" + $("#email_check").attr("name").replace(
+          /^(.+)_check$/, "$1") + "]").val())) {
+        show_error("が異なります", $("#email_check"));
+      }
+      //電話番号の検証
+      var tel = $.trim($("#tel").val());
+      if (tel && !(/^\(?\d{2,5}\)?[-(\.\s]{0,2}\d{1,4}[-)\.\s]{0,2}\d{3,4}$/gi).test(tel)) {
+        $("#tel").after("<p class='error'>電話番号の形式が異なります（半角英数字でご入力ください）</p>");
       }
 
-      //フォームが送信される際のイベントハンドラの設定
-      $("#main_contact").submit(function() {
-        //エラー表示の初期化
-        $("p.error").remove();
-        $("div").removeClass("error");
-        var text = "";
-        $("#errorDispaly").remove();
-
-        //メールアドレスの検証
-        var email = $.trim($("#email").val());
-        if (email && !(/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/gi).test(email)) {
-          $("#email").after("<p class='error'>メールアドレスの形式が異なります</p>");
-        }
-        //確認用メールアドレスの検証
-        var email_check = $.trim($("#email_check").val());
-        if (email_check && email_check != $.trim($("input[name=" + $("#email_check").attr("name").replace(
-            /^(.+)_check$/, "$1") + "]").val())) {
-          show_error("が異なります", $("#email_check"));
-        }
-        //電話番号の検証
-        var tel = $.trim($("#tel").val());
-        if (tel && !(/^\(?\d{2,5}\)?[-(\.\s]{0,2}\d{1,4}[-)\.\s]{0,2}\d{3,4}$/gi).test(tel)) {
-          $("#tel").after("<p class='error'>電話番号の形式が異なります（半角英数字でご入力ください）</p>");
-        }
-
-        //1行テキスト入力フォームとテキストエリアの検証
-        $(":text,textarea").filter(".validate").each(function() {
-          //必須項目の検証
-          $(this).filter(".required").each(function() {
-            if ($(this).val() == "") {
-              show_error(" は必須項目です", $(this));
-            }
-          });
-          //文字数の検証
-          $(this).filter(".max30").each(function() {
-            if ($(this).val().length > 30) {
-              show_error(" は30文字以内です", $(this));
-            }
-          });
-          $(this).filter(".max50").each(function() {
-            if ($(this).val().length > 50) {
-              show_error(" は50文字以内です", $(this));
-            }
-          });
-          $(this).filter(".max100").each(function() {
-            if ($(this).val().length > 100) {
-              show_error(" は100文字以内です", $(this));
-            }
-          });
-          //文字数の検証
-          $(this).filter(".max1000").each(function() {
-            if ($(this).val().length > 1000) {
-              show_error(" は1000文字以内でお願いします", $(this));
-            }
-          });
+      //1行テキスト入力フォームとテキストエリアの検証
+      $(":text,textarea").filter(".validate").each(function() {
+        //必須項目の検証
+        $(this).filter(".required").each(function() {
+          if ($(this).val() == "") {
+            show_error(" は必須項目です", $(this));
+          }
         });
-
-        //error クラスの追加の処理
-        if ($("p.error").length > 0) {
-          $("p.error").parent().addClass("error");
-          $('html,body').animate({
-            scrollTop: $("p.error:first").offset().top - 180
-          }, 'slow');
-          return false;
-        }
+        //文字数の検証
+        $(this).filter(".max30").each(function() {
+          if ($(this).val().length > 30) {
+            show_error(" は30文字以内です", $(this));
+          }
+        });
+        $(this).filter(".max50").each(function() {
+          if ($(this).val().length > 50) {
+            show_error(" は50文字以内です", $(this));
+          }
+        });
+        $(this).filter(".max100").each(function() {
+          if ($(this).val().length > 100) {
+            show_error(" は100文字以内です", $(this));
+          }
+        });
+        //文字数の検証
+        $(this).filter(".max1000").each(function() {
+          if ($(this).val().length > 1000) {
+            show_error(" は1000文字以内でお願いします", $(this));
+          }
+        });
       });
 
-      //テキストエリアに入力された文字数を表示
-      $("textarea").on('keydown keyup change', function() {
-        var count = $(this).val().length;
-        $("#count").text(count);
-        if (count > 1000) {
-          $("#count").css({
-            color: 'red',
-            fontWeight: 'bold'
-          });
-        } else {
-          $("#count").css({
-            color: '#333',
-            fontWeight: 'normal'
-          });
-        }
-      });
-    })
-    </script>
+      //error クラスの追加の処理
+      if ($("p.error").length > 0) {
+        $("p.error").parent().addClass("error");
+        $('html,body').animate({
+          scrollTop: $("p.error:first").offset().top - 180
+        }, 'slow');
+        return false;
+      }
+    });
+
+    //テキストエリアに入力された文字数を表示
+    $("textarea").on('keydown keyup change', function() {
+      var count = $(this).val().length;
+      $("#count").text(count);
+      if (count > 1000) {
+        $("#count").css({
+          color: 'red',
+          fontWeight: 'bold'
+        });
+      } else {
+        $("#count").css({
+          color: '#333',
+          fontWeight: 'normal'
+        });
+      }
+    });
+  })
+  </script>
 </body>
 
 </html>
